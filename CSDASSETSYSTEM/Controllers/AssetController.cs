b4dc28c799ee62ac4027dbc5c206eecafde55de4
Marieniel <mtcandelaria@civicmdsg.com.ph>
2023-01-12 15:19:02 +0800

    for quiz to , discuss actionlink

--- CSDASSETSYSTEM/Controllers/AssetController.cs
+++ CSDASSETSYSTEM/Controllers/AssetController.cs
@@ -10,6 +10,39 @@ namespace CSDASSETSYSTEM.Controllers
 {
     public class AssetController : Controller
     {
+
+
+        //PROJECT 2
+
+        private readonly List<Department> dept = new List<Department>()
+        {
+            new Department(){ DepartmentCode= "CSD", DepartmentName = "COMPUTER" },
+            new Department(){ DepartmentCode= "HR", DepartmentName = "HUMAN RESOURCES" },
+            new Department(){ DepartmentCode= "OTP", DepartmentName = "OFFICE OF THE PRESIDENT" },
+            new Department(){ DepartmentCode= "FINANCE", DepartmentName = "FINANCIALS" },
+
+
+        };
+
+
+        private readonly List<Person> persons = new List<Person>() {
+               new Person(){ Id=1,Department="CSD", CompanyID = "2282", Name = "Bryan" },
+               new Person(){Id=2, Department="FINANCE", CompanyID = "22821", Name = "JErich" },
+                new Person(){Id=3, Department="OTP", CompanyID = "22822", Name = "Marieniel" },
+                new Person(){Id=4, Department="HR", CompanyID = "22823", Name = "Risty" },
+                 new Person(){Id=5, Department="HR", CompanyID = "22823", Name = "Reymark" },
+                  new Person(){Id=6, Department="HR", CompanyID = "22823", Name = "Jayson" },
+                   new Person(){Id=7, Department="HR", CompanyID = "22823", Name = "Jessie" },
+                    new Person(){Id=8, Department="HR", CompanyID = "22823", Name = "Tommuel" },
+                    new Person(){Id=9, Department="HR", CompanyID = "22823", Name = "Henry" },
+                    new Person(){Id=10, Department="HR", CompanyID = "22823", Name = "Ian" },
+                    new Person(){Id=11, Department="HR", CompanyID = "22823", Name = "Jon" },
+            };
+
+
+
+
+
         // GET: Asset
         public ActionResult Index()
         {
@@ -54,29 +87,33 @@ namespace CSDASSETSYSTEM.Controllers
             ViewBag.ListOfPerson = persons;
             return View("Asset", asset);
         }
+
+
+
+     
         public ActionResult AssetDetailsWithViewBagAndModelWithParameter(int? Id, string Name)
         {
-            List<Person> persons = new List<Person>() {
-               new Person(){ Id=1,Department="CSD", CompanyID = "2282", Name = "Bryan" },
-               new Person(){Id=2, Department="FINANCE", CompanyID = "22821", Name = "JErich" },
-                new Person(){Id=3, Department="OTP", CompanyID = "22822", Name = "Marieniel" },
-                new Person(){Id=4, Department="HR", CompanyID = "22823", Name = "Risty" },
-                 new Person(){Id=5, Department="HR", CompanyID = "22823", Name = "Reymark" },
-                  new Person(){Id=6, Department="HR", CompanyID = "22823", Name = "Jayson" },
-                   new Person(){Id=7, Department="HR", CompanyID = "22823", Name = "Jessie" },
-                    new Person(){Id=8, Department="HR", CompanyID = "22823", Name = "Tommuel" },
-                    new Person(){Id=9, Department="HR", CompanyID = "22823", Name = "Henry" },
-                    new Person(){Id=10, Department="HR", CompanyID = "22823", Name = "Ian" },
-                    new Person(){Id=11, Department="HR", CompanyID = "22823", Name = "Jon" },
-            };
+           
             ViewBag.ListOfPerson = persons.Where(
-                per => 
-                (Id == null || per.Id == Id) &&
+                per =>
+                (Id == null || per.Id == Id) 
+                &&
                 (Name == null || per.Name.ToUpper().Contains(Name.ToUpper()))
-                
+
                 ).ToList();
             return View();
         }
+
+
+
+
+ 
+        public ActionResult UserDetails(int Id)
+        {
+            var person = persons.Where(x => x.Id == Id).FirstOrDefault();
+            return View(person);
+        }
+
 
 
 
